d1636bfbbba89ee660a69053cfdadcd243785321
WanglinSong <wsong14@hawk.iit.edu>
2019-01-16 17:29:40 -0600

    zip unzip

--- PA_1a/project-1a-unix-warmup-SongWanglin/my-unzip.c
+++ PA_1a/project-1a-unix-warmup-SongWanglin/my-unzip.c
@@ -1,7 +1,28 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char* argv[]){
 
-int main (int argc, char ** argv) {
-    return 0;
+    int number;
+    char chr;
+
+    if (argc == 1){ //no command-line arguments
+        printf("my-unzip: file1 [file2 ...]\n");
+        return 1;
+    }
+    for (int i = 1; i<argc; i++){ //go through all command line parameters except the first one
+        FILE *file;
+        file = fopen(argv[i], "rb");
+        if (!file){
+            printf("my-unzip: cannot open file\n");
+            return 1;
+        }
+        while(fread(&number, sizeof(int), 1, file)){ //put 4 bytes from file inside number variable
+            fread(&chr, sizeof(char), 1, file); //put 1 byte from file inside chr variable
+            for (int i=0; i<number; i++) //print chr number times
+                putc(chr, stdout);
+        }
+        fclose(file);
+    }
+    putc('\n', stdout);
 }
--- PA_1a/project-1a-unix-warmup-SongWanglin/my-zip.c
+++ PA_1a/project-1a-unix-warmup-SongWanglin/my-zip.c
@@ -1,7 +1,42 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char* argv[]){
 
-int main (int argc, char ** argv) {
-    return 0;
+    int buffer;
+    int temp;
+    int counter;
+    int lever;
+
+    if (argc == 1){ //no command-line arguments
+        printf("my-zip: file1 [file2 ...]\n");
+        return 1;
+    }
+    for (int i=1; i<argc; i++){ //go through all command line parameters except the first one
+        FILE *file;
+        file = fopen(argv[i], "r");
+        if (!file){
+            printf("my-zip: cannot open file\n");
+            return 1;
+        }
+        counter = 1;
+        lever = 0;
+        while ((buffer = fgetc(file)) != EOF){ //take one character from file
+            if (!lever){ //only first cycle
+                lever++;
+                temp = buffer; //save character to temp
+            }
+            else if (lever){ //all other cycles
+                if (buffer == temp) //check if character is same as previous
+                    counter++;
+                else{ //if different character - write out previous character's number and character itself
+                    fwrite(&counter, sizeof(int), 1,  stdout);
+                    fwrite(&temp, sizeof(char), 1, stdout);
+                    counter = 1;
+                    temp = buffer; //save different character to temp
+                }
+            }
+        }
+        fclose(file);
+    }
 }
