93ce0e814e3e5199ed988f13d4804db929be4d4c
WanglinSong <wsong14@hawk.iit.edu>
2019-01-18 22:15:51 -0600

    comments

--- PA_1a/project-1a-unix-warmup-SongWanglin/my-unzip.c
+++ PA_1a/project-1a-unix-warmup-SongWanglin/my-unzip.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[]){
-
-    int number[1]; // how many times the character repeat
-    char chr[1]; //the repeated character
+int main(int argc, char** argv){
+    int number = 0; // how many times the character repeat
+    char chr = '\0'; //the repeated character
     FILE *fp;
-
-    if (argc == 1){ 
+    /* check if file names are provided */
+    if (argc == 1){
         printf("my-unzip: file1 [file2 ...]\n");
         return 1;
     }
+    /
     for (int i = 1; i<argc; i++){ 
-        fp = fopen(argv[i], "r");
-        if (!fp){
+        fp = fopen(*(argv+i), "r"); // open **argv one by one
+	/* check if file names are valid */
+        if (!fp){	
             printf("my-unzip: cannot open file\n");
             return 1;
         }
-        while(fread(&number, sizeof(int), 1, fp)){ //put 4 bytes from file inside number variable
-            if( fread(&chr, sizeof(char), 1, fp)!=1)
+	/* unzip */
+        while(fread(&number, sizeof(int), 1, fp)){ // put a 4B integer from file inside number variable, break if fail
+            if( fread(&chr, sizeof(char), 1, fp)!=sizeof(char)) // break if can't read  a character from file
 		    break;
-	    for (int i=0; i<number[0]; i++)
-		    putc(chr[0], stdout);
+	    for (int i=0; i<number; i++) 
+		    putc(chr, stdout);
         }
         fclose(fp);
     }
--- PA_1a/project-1a-unix-warmup-SongWanglin/my-zip.c
+++ PA_1a/project-1a-unix-warmup-SongWanglin/my-zip.c
@@ -2,35 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char* argv[]){
-    char last = '\0', current = '\0';
+int main(int argc, char** argv){
+    char last = '\0', current = '\0'; // 2 counters to search and then make changes (2-pointer method)
     int count = 1;  //how many times a character appear
     FILE *fp;
-    if (argc == 1){ //no command-line arguments
+
+    /* check if argv provided any file names */
+    if (argc == 1){ 
         printf("my-zip: file1 [file2 ...]\n");
-        return 1;
+        exit(1);
     }
-    for (int i=1; i<argc; i++){ //go through all command line parameters except the first one
+
+    for (int i=1; i<argc; i++){ //go through all *argv[i] except the first one (which is supposed to be ./my-zip)
         fp = fopen(*(argv+i), "r");
-        if (fp == NULL){
+
+	/* check if this filename is valid */
+        if (!fp){   
             printf("my-zip: cannot open file\n");
-            return 1;
+            exit(1);
         }
         if (last == '\0')
-            last = getc(fp);
-        while( (current = getc(fp))!= EOF){
-            if(current==last){
-                count++;
-            } else{
-                fwrite(&count, 4, 1, stdout);
-                fwrite(&last, 1,  1, stdout);
+            last = getc(fp);  // get the character from file stream, return the corresponding ASCII or EOF
+	
+	/* 2-pointer tracking */
+        while( (current = getc(fp))!= EOF) { // use "current" counter to search
+            if(current==last){  
+                count++;    // count the same character
+            } else{         // write to the output stream if we found the next different character, reset the counter to 1
+                fwrite(&count, sizeof(int), 1, stdout);
+                fwrite(&last, sizeof(char),  1, stdout);
                 count = 1;
             }
             last = current;
-        }
-        if( i == argc-1){
-            fwrite(&count, 4, 1, stdout);
-            fwrite(&last, 1, 1, stdout);
+        } 
+
+	/* check if it is the last char in  the last file, if so, write what's still stored in the current tracker */
+	/* (the last character of other files is already handled by the previous while loop) */
+        if( i == argc-1){ 
+            fwrite(&count, sizeof(int), 1, stdout);
+            fwrite(&last, sizeof(char), 1, stdout);
             count = 1;
         }
         fclose(fp);
